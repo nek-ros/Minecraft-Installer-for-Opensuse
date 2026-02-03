b21d31d465f7e8fc461fc4758e794608640f22a7
nek-ros <5xeprzcrvi@gmail.com>
2026-02-03 14:44:40 -0300

    1.0.v - stable

--- install-mine.sh
+++ install-mine.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+
 # Cores para facilitar a leitura
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
+
 echo -e "${GREEN}Iniciando a instalação do Minecraft...${NC}"
+
 
 # 1. Verificar se o usuário é root
 if [ "$EUID" -ne 0 ]; then 
   echo -e "${RED}Erro: Por favor, execute como root (use sudo).${NC}"
   exit 1
 fi
+
 
 # 2. Atualizar repositórios e instalar dependências
 echo -e "Atualizando pacotes e instalando wget/gzip..."
@@ -20,12 +24,12 @@ zypper install -y wget gzip
 sleep 2
 
 
-
 # 3. Limpar instalações antigas e preparar diretório
 echo -e "Limpando diretórios antigos..."
 rm -rf /opt/minecraft-launcher
 rm -f /usr/bin/minecraft-launcher
 mkdir -p /opt/minecraft-launcher
+
 
 # 4. Baixando Minecraft
 echo -e "Baixando o launcher oficial"
@@ -35,8 +39,7 @@ sleep 2
 mv /tmp/Minecraft-Installer-for-Opensuse/src/launcher.png /opt/minecraft-launcher/
 sleep 2
 wget https://launcher.mojang.com/download/Minecraft.tar.gz
-
-
+sleep 2
 
 
 # 5. Descompactar
@@ -50,12 +53,13 @@ mv minecraft-launcher/* /opt/minecraft-launcher/
 # Isso faz com que o comando 'minecraft-launcher' funcione no terminal
 ln -sf /opt/minecraft-launcher/minecraft-launcher /usr/bin/minecraft-launcher
 
+
 # 7. Criar o arquivo .desktop (O seu erro de ícone estava aqui)
 echo -e "Criando atalho no menu de aplicativos..."
 cat <<EOF > /usr/share/applications/Minecraft.desktop
 [Desktop Entry]
 Name=Minecraft
-Comment=Launcher oficial do Minecraft
+Comment=Launcher Unoficial do Minecraft
 GenericName=Game
 Exec=/usr/bin/minecraft-launcher
 Icon=/opt/minecraft-launcher/launcher.png
@@ -66,12 +70,15 @@ Keywords=mojang;minecraft;launcher;
 StartupNotify=true
 EOF
 
+
 # 8. Ajustar permissões (Essencial para o GNOME mostrar o ícone)
 chmod +x /usr/share/applications/Minecraft.desktop
 chmod +x /usr/bin/minecraft-launcher
 
+
 # 9. Atualizar base de dados de aplicativos
 update-desktop-database /usr/share/applications/
+
 
 echo -e "${GREEN}-----------------------------------------${NC}"
 echo -e "${GREEN}Instalação concluída com sucesso!${NC}"
